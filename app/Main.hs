7adeda7248f1a0fec6cd60ff92bf7382baa9cd99
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-11-09 20:12:34 -0500

    Adding basic driver

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import Data.Semigroup (Arg (Arg))
+import System.Environment
+
+solver :: [String] -> [String]
+solver (x : xs) = ["Solution1", "Solution2"] --Corresponding to their algorithms
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  print "Welcome to Sudoku Solver!"
+  args <- getArgs
+  if null args
+    then do
+      error "Please enter a sudoku puzzle and an algorithm."
+    else
+      if length args == 1
+        then
+          if head args == "--help"
+            then do
+              putStrLn "Usage: sudoku-solver <puzzle> <algorithm1> <algorithm2> ..."
+              putStrLn "Algorithms: Basic, ..."
+            else do
+              error "Please enter a sudoku puzzle and an algorithm."
+        else do
+          let solutions = solver args
+          print solutions
