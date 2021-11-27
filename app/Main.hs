b546922cc7d2970ee7a2690595a82c74517b5e3d
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-11-27 15:07:07 -0500

    Adding generate and solve mode with command line input parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,31 @@
 module Main where
-
-
 import Sudoku
 import Control.Monad
 import GenerateSudoku
-
+import System.Environment ( getArgs )
 --cabal run sudoku-solver.cabal
 
 
 main :: IO ()
 main = do
   putStrLn "\n\n **** SUDOKU SOLVER ****\n"
-  putStrLn "\nGenerate Random Sudoku: \n"
-  b <- return $ genBoard 30
-  putStrLn $ showBoard b
-  putStrLn "\nSalida a interface : \n"
-  input <- return $ boardToString b
-  putStrLn $ input
-  putStrLn "\nSolve Sudoku: \n"
-  case readGrid input of
-    Nothing   -> putStrLn "Invalid input"
-    Just grid -> case Sudoku.solve grid of
-        Nothing    -> putStrLn "No solution found"
-        Just grid' -> putStrLn $ showGrid grid' ++
-         "\nSalida a interface : \n\n" ++ showGridConcat grid'
-
+  --Commandline arguments
+  args <- getArgs
+  --If no arguments are given, generate a sudoku
+  if null args then do
+    putStrLn "\nGenerate Random Sudoku: \n"
+    let b = genBoard 30
+    putStrLn $ showBoard b
+    putStrLn "\nSalida a interface : \n"
+    let input = boardToString b
+    putStrLn input
+  --If arguments are given, solve the sudoku
+  else do
+    putStrLn "\nSolve Sudoku: \n"
+    let input = head args
+    case readGrid input of
+      Nothing   -> putStrLn "Invalid input"
+      Just grid -> case Sudoku.solve grid of
+          Nothing    -> putStrLn "No solution found"
+          Just grid' -> putStrLn $ showGrid grid' ++
+           "\nSalida a interface : \n\n" ++ showGridConcat grid'
